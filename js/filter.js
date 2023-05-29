14421a483396b91e15156452c90010d3e82c5e5e
Kateryna1706 <silckatya@gmail.com>
2023-05-29 19:51:56 +0200

    added links to buttons

---
+++ js/filter.js
@@ -0,0 +1,7 @@
+const refs = {
+all: document.querySelector(".all"),
+webSite: document.querySelector(".veb-site"),
+app: document.querySelector(".app"),
+design: document.querySelector(".design"),
+marketing: document.querySelector(".marketing"),
+}
